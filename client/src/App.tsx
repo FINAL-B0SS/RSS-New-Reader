39f77134369be4752f799af48a3a280bc0681f7b
FINAL-B0SS <1malik.jean@gmail.com>
2021-01-10 22:57:28 -0800

    Implement pagination and page navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,11 @@ import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
 import SearchField from './components/SearchField'
 import FeedInfoCard from './components/FeedInfoCard'
+import ItemInfoCard from './components/ItemInfoCard'
 import { makeStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
+import PageNavigation from './components/PageNavigation'
 import { Item, Feed, fetchFeedInfo, fetchFeedItems } from './API'
-import ItemInfoCard from './components/ItemInfoCard'
 
 const useStyles = makeStyles((theme) => ({
 	title: {
@@ -21,6 +22,7 @@ const useStyles = makeStyles((theme) => ({
 const App: React.FC = () => {
 	const [feeds, setFeeds] = useState<Feed[]>([])
 	const [items, setItems] = useState<Item[]>([])
+	const [page, setPage] = useState<number>(0)
 
 	const classes = useStyles()
 
@@ -38,21 +40,42 @@ const App: React.FC = () => {
 		}
 	}
 
+	let pageCount = Math.ceil(
+		(items.length > 0 ? items.length : feeds.length) / 5
+	)
+
+	const pageNavigation = (option: string) => {
+		if (option === '.') {
+			setPage(0)
+			setItems([])
+		} else if (option === '+' && page + 1 < pageCount) {
+			setPage((prev) => prev + 1)
+		} else if (option === '-' && page !== 0) {
+			setPage((prev) => prev - 1)
+		}
+	}
+
 	const feedInfoCards = feeds.map((element) => (
 		<FeedInfoCard key={element.link} feed={element} callback={fetchItems} />
 	))
 
-	const ItemInfoCards = items.map((element) => (
-		<ItemInfoCard key={element.link} item={element} />
-	))
+	const ItemInfoCards = items
+		.slice(page, page + 5)
+		.map((element) => <ItemInfoCard key={element.link} item={element} />)
 
 	return (
 		<Box className='App'>
 			<Grid container direction='column' alignItems='center'>
 				<Typography className={classes.title}>My News Reader</Typography>
-				<SearchField callback={fetchFeed} />
+				{items.length === 0 && <SearchField callback={fetchFeed} />}
 				{feeds.length > 0 && items.length === 0 && feedInfoCards}
 				{items.length > 0 && ItemInfoCards}
+				<PageNavigation
+					callback={pageNavigation}
+					page={page}
+					pageCount={pageCount}
+					isHome={items.length === 0}
+				/>
 			</Grid>
 		</Box>
 	)
---
+++ client/src/components/PageNavigation.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import Box from '@material-ui/core/Box'
+import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
+
+type Props = {
+	callback: (option: string) => void
+	page: number
+	pageCount: number
+	isHome: boolean
+}
+
+const useStyles = makeStyles((theme) => ({
+	searchButton: {
+		fontSize: '1rem',
+		justify: 'right',
+		color: '#FFFFFF',
+		marginLeft: '1rem',
+		border: '2px solid black',
+		backgroundColor: '#6441A5',
+		width: theme.spacing(10),
+		height: theme.spacing(7),
+		borderRadius: theme.spacing(1),
+	},
+	box: {
+		width: theme.spacing(85),
+		marginBottom: theme.spacing(1),
+		display: 'flex',
+		justifyContent: 'flex-end',
+	},
+	pageCounter: {
+		fontWeight: 'bold',
+		marginRight: theme.spacing(11),
+	},
+}))
+
+const PageNavigation: React.FC<Props> = ({
+	callback,
+	page,
+	pageCount,
+	isHome,
+}) => {
+	const classes = useStyles()
+
+	return (
+		<Grid>
+			<Grid container justify='flex-end'>
+				<Grid container direction='row' justify='center'>
+					<Box className={classes.box}>
+						{!isHome && (
+							<Button
+								onClick={() => callback('.')}
+								className={classes.searchButton}
+							>
+								Home
+							</Button>
+						)}
+						<Button
+							onClick={() => callback('-')}
+							className={classes.searchButton}
+						>
+							Prev
+						</Button>
+						<Button
+							onClick={() => callback('+')}
+							className={classes.searchButton}
+						>
+							Next
+						</Button>
+					</Box>
+				</Grid>
+				<span className={classes.pageCounter}>
+					Page: {page + 1} / {pageCount}
+				</span>
+			</Grid>
+		</Grid>
+	)
+}
+
+export default PageNavigation
