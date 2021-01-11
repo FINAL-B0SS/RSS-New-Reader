e2298eace24beb8038aae89bec89a4789fb2491e
FINAL-B0SS <1malik.jean@gmail.com>
2021-01-10 17:39:03 -0800

    Create method for fetching a feeds items

--- client/src/API.ts
+++ client/src/API.ts
@@ -6,6 +6,15 @@ export type Feed = {
 	image?: string
 }
 
+export type Item = {
+	title: string
+	description: string
+	image: string
+	link: string
+	pubDate: string
+	'content:encoded': string
+}
+
 export const fetchFeedInfo = async (link: string, feeds: Feed[]) => {
 	if (!feeds.find((element) => element.link === link)) {
 		const endpoint = `http://localhost:8080/api/fetchFeed?link=${link}`
@@ -23,4 +32,15 @@ export const fetchFeedInfo = async (link: string, feeds: Feed[]) => {
 	} else {
 		console.error('Feed already added!')
 	}
+}
+
+export const fetchFeedItems = async (link: string, feeds: Feed[]) => {
+	const endpoint = `http://localhost:8080/api/fetchItems?link=${link}`
+
+	let data = await fetch(endpoint)
+		.then((res) => res.json())
+		.then((res) => {
+			return res
+		})
+	return data
 }
